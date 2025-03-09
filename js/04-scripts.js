0c8fbcbc3a67a83ff2f66fec9cd8993b809f7a42
Fernando Saucedo <contactofernandosaucedo@gmail.com>
2025-03-09 11:37:58 -0300

    Query selector

---
+++ js/04-scripts.js
@@ -0,0 +1,19 @@
+const card = document.querySelector('.card');
+console.log(card);
+
+
+// Podemos tener selectores especificos como en CSS
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+// Selecionar el segundo card de hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+// Seleccionar el formulario conj QuerySelector
+const formulario = document.querySelector('#formulario');
+console.log(formulario);
+
+// Selecionar elementos HTML
+const navegacion = document.querySelector('nav');
+console.log(navegacion)
